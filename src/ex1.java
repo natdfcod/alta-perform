f15d83c3b5b35bb072aea31a89c2f27cac6e8cc3
Laboratório FIAP <labsfiap@fiap.com.br>
2026-02-23 09:05:34 -0300

    intacia da media depois

--- src/ex1.java
+++ src/ex1.java
@@ -4,40 +4,38 @@ public class ex1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         final int N = 30;
-        int [] rm = new int[N];
-        double [] nota1 = new double[N];
-        double [] nota2 = new double[N];
-        double [] media = new double[N];
-        String [] status = new String[N];
+        int n = 0;
+        int[] rm = new int[N];
+        double[] nota1 = new double[N];
+        double[] nota2 = new double[N];
+        String[] status = new String[N];
 
         //receber dados
-        for(int i=0;i<N;i++){
         System.out.println("Digite seu RM: ");
-        rm[i] = sc.nextInt();
-        //verificar rm
-        if(rm[i]<0){
-            break;
+        int rmLido = sc.nextInt();
+        while (rmLido >= 0 && n < N) {
+            rm[n] = rmLido;
+            System.out.println("Digite sua nota1: ");
+            nota1[n] = sc.nextDouble();
+            System.out.println("Digite sua nota2: ");
+            nota2[n] = sc.nextDouble();
+            n++;
+            System.out.println("Digite seu RM: ");
+            rmLido = sc.nextInt();
         }
-        System.out.println("Digite sua nota1: ");
-        nota1[i] = sc.nextDouble();
-        System.out.println("Digite sua nota2: ");
-        nota2[i] = sc.nextDouble();
-        //media
-        media[i] = (nota1[i] + nota2[i])/2;
-        //gerear aprovado e reprovado
-        if (media[i] >= 6){
-            status[i] = "Aprovado";
-        } else {
-            status[i] = "Reprovado";
-        }
-        }
-        //apresentar dados
-        for(int i=0;i<N;i++){
-            if(rm[i] >= 0){
-            System.out.printf("RM: %d | Média: %.1f | Status: %s\n", rm[i], media[i], status[i]);
+        double[] media = new double[n];
+        for(int i = 0; i < n; i++){
+            //media
+            media[i] = (nota1[i]+nota2[i])/2;
+            //gerear aprovado e reprovado
+            if (media[i] >= 6) {
+                status[i] = "Aprovado";
             } else {
-                break;
+                status[i] = "Reprovado";
             }
+            //apresentar dados
+            System.out.printf("RM: %d | Média: %.1f | Status: %s\n", rm[i], media[i], status[i]);
         }
     }
 }
+
